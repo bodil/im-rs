a8cc0118b4e173c80bc79e17aac3c227986e76f0
Bodil Stokke <bodil@bodil.org>
2018-01-29 13:02:12 +0000

    Document `SharedHasher`.

--- src/hash.rs
+++ src/hash.rs
@@ -1,3 +1,5 @@
+//! Tools for hashing.
+
 use std::sync::Arc;
 use std::collections::hash_map::RandomState;
 use std::hash::BuildHasher;
@@ -6,7 +8,16 @@ lazy_static! {
     static ref DEFAULT_HASHER: Arc<RandomState> = Arc::new(RandomState::new());
 }
 
+/// A trait for acquiring a global shared `BuildHasher` instance.
+///
+/// This is desirable because, unlike mutable data structures, persistent
+/// data structures can share data between them. Shared subtrees of hash maps
+/// must be using the same hasher to be compatible with each other, and so
+/// this trait is provided to make that easier to accomplish without increased
+/// boilerplate and one more thing to keep track of.
 pub trait SharedHasher: BuildHasher {
+    /// Get a reference to a global instance of `BuildHasher` for the
+    /// hashing algorithm we're implementing.
     fn shared_hasher() -> Arc<Self>;
 }
 
