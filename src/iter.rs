903b3c820e152e4bdf7cb2f2c0c3906d13373d64
Bodil Stokke <bodil@bodil.org>
2018-03-25 15:52:44 +0100

    Mutable unfold.

--- src/iter.rs
+++ src/iter.rs
@@ -22,6 +22,22 @@ where
     }
 }
 
+pub struct UnfoldMut<F, S> {
+    f: F,
+    value: S,
+}
+
+impl<F, S, A> Iterator for UnfoldMut<F, S>
+where
+    F: Fn(&mut S) -> Option<A>,
+{
+    type Item = A;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.f)(&mut self.value)
+    }
+}
+
 /// Create an iterator of values using a function to update
 /// a state value.
 ///
@@ -52,4 +68,43 @@ where
     F: Fn(&S) -> Option<(A, S)>,
 {
     Unfold { f, value }
+}
+
+/// Create an iterator of values using a function to mutate
+/// a state value.
+///
+/// The function is called with a mutable reference to the current state as its
+/// argument, and should return an [`Option`][std::option::Option] of the
+/// next value to yield from the iterator, updating the state as necessary.
+/// If the function returns [`None`][std::option::Option::None], the iterator ends.
+///
+/// This differs from [`unfold`] in that your update functions will probably be
+/// less elegant, but it's easier to deal with state that isn't efficiently cloneable.
+///
+/// # Examples
+/// ```
+/// # #[macro_use] extern crate im;
+/// # use im::iter::unfold_mut;
+/// # use im::catlist::CatList;
+/// # fn main() {
+/// // Create an infinite stream of numbers, starting at 0.
+/// let mut it = unfold_mut(0, |i| {
+///   let next = *i;
+///   *i += 1;
+///   Some(next)
+/// });
+///
+/// // Make a list out of its first five elements.
+/// let numbers = CatList::from(it.take(5));
+/// assert_eq!(numbers, catlist![0, 1, 2, 3, 4]);
+/// # }
+/// ```
+///
+/// [std::option::Option]: https://doc.rust-lang.org/std/option/enum.Option.html
+/// [std::option::Option::None]: https://doc.rust-lang.org/std/option/enum.Option.html#variant.None
+pub fn unfold_mut<F, S, A>(value: S, f: F) -> UnfoldMut<F, S>
+where
+    F: Fn(&mut S) -> Option<A>,
+{
+    UnfoldMut { f, value }
 }
