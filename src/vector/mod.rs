32f58e914c84a6e965eea8e732a81cc3cd160fbb
Bodil Stokke <bodil@bodil.org>
2018-04-03 17:06:36 +0100

    Relicense to MPL2.

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //! A vector.
 //!
 //! This is an implementation of [bitmapped vector tries][bmvt], which
