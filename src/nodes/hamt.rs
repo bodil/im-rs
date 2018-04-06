cfc430ba0ec482b06bcb510083bc79f8b6c18b07
Bodil Stokke <bodil@bodil.org>
2018-04-06 16:50:02 +0100

    Unused configuration in nodes/hamt.

--- src/nodes/hamt.rs
+++ src/nodes/hamt.rs
@@ -2,11 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#![cfg_attr(feature = "clippy", allow(new_ret_no_self))]
-
-// use std::fmt::{Debug, Error, Formatter};
-// use std::hash::{BuildHasher, Hash, Hasher};
-// use std::ptr;
 use std::sync::Arc;
 
 use bits::{bitpos, index, Bitmap, HASH_BITS, HASH_SIZE};
