8ad9cae823263efbd5523df2749472a948c06e3c
Bodil Stokke <bodil@bodil.org>
2018-04-09 14:08:53 +0100

    Increase HAMT branch size to 64.
    Seems to give the best overall performance, as simply cloning native
    data structures is generally faster up to around 100 elements so we
    want to extend that benefit to HAMT's tails as much as we can, and a
    higher branching factor gives better memory performance.

--- src/bits.rs
+++ src/bits.rs
@@ -6,11 +6,11 @@
 
 use std::hash::{BuildHasher, Hash, Hasher};
 
-pub type Bitmap = u16; // a uint of HASH_SIZE bits
-pub const HASH_BITS: usize = 4;
+pub type Bitmap = u64; // a uint of HASH_SIZE bits
+pub const HASH_BITS: usize = 6;
 pub const HASH_SIZE: usize = 1 << HASH_BITS;
 pub const HASH_MASK: Bitmap = (HASH_SIZE - 1) as Bitmap;
-pub const HASH_COERCE: u64 = ((1 << HASH_SIZE as u64) - 1);
+//pub const HASH_COERCE: u64 = ((1 << HASH_SIZE as u64) - 1);
 
 #[inline]
 pub fn mask(hash: Bitmap, shift: usize) -> Bitmap {
@@ -30,5 +30,6 @@ pub fn index(bitmap: Bitmap, bit: Bitmap) -> usize {
 pub fn hash_key<K: Hash, S: BuildHasher>(bh: &S, key: &K) -> Bitmap {
     let mut hasher = bh.build_hasher();
     key.hash(&mut hasher);
-    (hasher.finish() & HASH_COERCE) as Bitmap
+    // (hasher.finish() & HASH_COERCE) as Bitmap
+    hasher.finish() as Bitmap
 }
