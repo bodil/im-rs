8751ab240dfe8068a55a8143da84864e874d8e48
Alex Crichton <alex@alexcrichton.com>
2019-09-17 12:54:10 -0700

    Switch to `version_check` from `rustc_version`
    Currently `rustc_version` has a dependency on `semver`, which can
    sometimes be a bit of a weighty crate to build (if its `serde` feature
    is enabled). This instead switches to a similar crate, `version_check`,
    to check in the build script to see if it's a nightly rustc compiler.

--- build.rs
+++ build.rs
@@ -2,18 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-extern crate rustc_version;
-
-use rustc_version::{version_meta, Channel};
 use std::env;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    match version_meta().unwrap().channel {
-        Channel::Nightly => {
+    if let Some(channel) = version_check::Channel::read() {
+        if channel.supports_features() {
             println!("cargo:rustc-cfg=has_specialisation");
         }
-        _ => (),
     }
     let pkgname = env::var("CARGO_PKG_NAME").expect("Cargo didn't set the CARGO_PKG_NAME env var!");
     let test_rc = env::var("IM_TEST_RC").is_ok();
