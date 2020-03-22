106667ac2b543fd23bb45bf1ecb849a86d8a1603
Julian Kenwood <nomad010@gmail.com>
2020-03-22 21:02:30 +0200

    Reduce insert test size

--- src/tests/vector.rs
+++ src/tests/vector.rs
@@ -220,11 +220,10 @@ proptest! {
 
 #[test]
 fn test_inserts() {
-    const N: usize = 200_000;
+    const N: usize = 2000;
     let mut v = Vector::new();
     for i in 0..N {
         v.insert(v.len() / 2, i);
-        println!("{}", i);
     }
     let mut rv: Vec<usize> = Vec::new();
     rv.extend((0..N).skip(1).step_by(2));
