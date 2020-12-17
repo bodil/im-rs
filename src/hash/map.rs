ab09f1863ee106f2fd0a2c47f77143773e684d4e
Simon Sapin <simon.sapin@exyr.org>
2020-12-17 20:56:55 +0100

    Build union of two maps/sets by mutating the larger one, not the first
    `fn union(self, other: Self) -> Self` is implemented by progressively
    mutating then returning one of the two given collections,
    while consuming and iterating the other.
    
    If one of the two collections is much larger,
    mutating that one can be faster and yield better reuse of internal nodes.

--- src/hash/map.rs
+++ src/hash/map.rs
@@ -932,11 +932,16 @@ where
     /// assert_eq!(expected, map1.union(map2));
     /// ```
     #[must_use]
-    pub fn union(mut self, other: Self) -> Self {
-        for (k, v) in other {
-            self.entry(k).or_insert(v);
+    pub fn union(self, other: Self) -> Self {
+        let (mut to_mutate, to_consume) = if self.len() >= other.len() {
+            (self, other)
+        } else {
+            (other, self)
+        };
+        for (k, v) in to_consume {
+            to_mutate.entry(k).or_insert(v);
         }
-        self
+        to_mutate
     }
 
     /// Construct the union of two maps, using a function to decide
@@ -982,7 +987,20 @@ where
     /// ));
     /// ```
     #[must_use]
-    pub fn union_with_key<F>(mut self, other: Self, mut f: F) -> Self
+    pub fn union_with_key<F>(self, other: Self, mut f: F) -> Self
+    where
+        F: FnMut(&K, V, V) -> V,
+    {
+        if self.len() >= other.len() {
+            self.union_with_key_inner(other, f)
+        } else {
+            other.union_with_key_inner(self, |key, other_value, self_value| {
+                f(key, self_value, other_value)
+            })
+        }
+    }
+
+    fn union_with_key_inner<F>(mut self, other: Self, mut f: F) -> Self
     where
         F: FnMut(&K, V, V) -> V,
     {
